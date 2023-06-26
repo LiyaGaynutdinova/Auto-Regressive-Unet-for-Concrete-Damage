60c9e6b6dfe97c6015dd9e4b43e43fb3e9376414
LiyaGaynutdinova <104518595+LiyaGaynutdinova@users.noreply.github.com>
2023-06-26 23:50:27 +0200

    first unet implementation

---
+++ dataset.py
@@ -0,0 +1,64 @@
+import os
+import glob
+from PIL import Image, ImageOps
+import numpy as np
+import torch
+import csv
+from torchvision import datasets, transforms
+from torch.utils.data import Dataset, DataLoader
+
+class dataset(Dataset):
+    def __init__(self, res, type=None):
+        if type=='simple':
+            self.imgs_path = "reduxed_results/reduxed_results/geometry/"
+            self.label_path = "reduxed_results/reduxed_results/damage_fields/"
+            input_path = [self.imgs_path + str(x) + '.npy' for x in range(15000)]
+            output_path = [self.label_path + str(x) + '_99.npy' for x in range(15000)]
+            self.data = []
+            for i in range(15000):
+                self.data.append([input_path[i], output_path[i]])
+                
+        else:
+            raise('Not implemented')
+        
+        self.img_res = 99
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        img_path, label_path = self.data[idx]
+        transform = torch.nn.Sequential(
+            transforms.RandomHorizontalFlip(p=0.5),
+            transforms.RandomVerticalFlip(p=0.5)
+        )       
+        img = torch.tensor(np.load(img_path)[:-1,:-1], dtype=torch.float)
+        label = torch.tensor(np.load(label_path)[:-1,:-1], dtype=torch.float)
+        tensor = torch.stack([img, label])
+        tensor = torch.unsqueeze(tensor,1)
+        tensor = transform(tensor)
+        k = np.random.rand()
+        if (k < 0.25):
+            tensor = transforms.functional.rotate(tensor, 90)
+        elif (k >= 0.25) and (k < 0.5):
+            tensor = transforms.functional.rotate(tensor, 180)
+        elif (k >= 0.5) and (k < 0.75):
+            tensor = transforms.functional.rotate(tensor, 270)
+        roll_x = np.random.randint(self.img_res)
+        roll_y = np.random.randint(self.img_res)
+        tensor = torch.roll(tensor, roll_x, -2)
+        tensor = torch.roll(tensor, roll_y, -1)
+        return tensor[0], tensor[1]
+
+
+def get_loaders(data, batch_size):
+    n_train = int(0.8 * data.__len__())
+    n_test = (data.__len__() - n_train) // 2
+    n_val = data.__len__() - n_train - n_test
+    torch.manual_seed(0)
+    train_set, val_set, test_set = torch.utils.data.random_split(data, [n_train, n_val, n_test])
+    train_loader = DataLoader(train_set, batch_size = batch_size, shuffle=True, num_workers=0)
+    val_loader = DataLoader(val_set, batch_size = batch_size)
+    test_loader = DataLoader(test_set, batch_size = n_test, shuffle=True)
+    loaders = {'train' : train_loader, 'val' : val_loader, 'test' : test_loader}
+    return loaders
---
+++ train_Unet.py
@@ -0,0 +1,82 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import matplotlib.pyplot as plt
+from save_load import *
+
+
+def plot_outputs(x, y, fname):
+    n_samples = len(x)
+    fig, axs = plt.subplots(nrows=2, ncols=n_samples, figsize=(n_samples, 2), dpi=100)
+    for i in range(n_samples):
+        axs.flat[i].imshow(x[i][0].detach().cpu().numpy(), cmap='Greys')
+        axs.flat[i].set_axis_off()
+    for i in range(n_samples, 2*n_samples):
+        axs.flat[i].imshow(y[i-n_samples][0].detach().cpu().numpy(), cmap='Greys')
+        axs.flat[i].set_axis_off()
+    plt.savefig(fname)
+    plt.close()
+
+
+def train(net, loaders, args):
+
+    # network
+    net.to(args['dev'])
+
+    # loss function
+    loss = nn.BCELoss(reduction='none')
+
+    # optimizer
+    optimizer = optim.Adam(net.parameters(), lr = args['lr'])
+
+    n_train = len(loaders['train'].dataset)
+    n_val = len(loaders['val'].dataset)
+
+    losses_train = []
+    losses_val = []
+
+    for epoch in range(args['epochs']):
+        if args['dev'] == "cuda":
+            torch.cuda.empty_cache() 
+        # accumulate total loss over the dataset
+        L = 0
+        net.train()
+        # loop fetching a mini-batch of data at each iteration
+        for i, (x, label) in enumerate(loaders['train']):
+            x = x.to(args['dev'])
+            # apply the network
+            y = net(x)
+            y_filtered = y * x.round()
+            # calculate mini-batch losses
+            l = loss(y_filtered, label).sum()
+            # accumulate the total loss as a regular float number
+            loss_batch = l.detach().item()
+            L += loss_batch
+            # the gradient usually accumulates, need to clear explicitly
+            optimizer.zero_grad()
+            # compute the gradient from the mini-batch loss
+            l.mean().backward()
+            # make the optimization step
+            optimizer.step()
+
+        # calculate the loss and accuracy of the validation set
+        net.eval()
+        if args['dev'] == "cuda":
+            torch.cuda.empty_cache() 
+        
+        L_val = 0
+        for j, (x_val, label_val) in enumerate(loaders['val']):
+            x_val = x_val.to(args['dev'])
+            y_val = net(x_val)
+            y_val_filtered = y * x.round()
+            L_val += loss(y_val_filtered, label_val).detach().sum().item()
+        losses_train.append(L / n_train)
+
+        print(f'Epoch: {epoch} mean train loss: {L / n_train : 5.10f} mean val. rec. loss: {L_val / n_val : 5.10f}')
+        if epoch % 5 == 0:
+            save_network(net, args['name'] + f'_{epoch}')
+            plot_outputs(net.encoder, net.decoder, x_val, y_val, args['name'] + f'_{epoch}')
+    save_network(net, args['name'])
+    plot_outputs(label_val, y_val, args['name'] + f'_{epoch}')
+
+    return losses_train, losses_val
